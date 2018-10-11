f6d7e0138f560e119963e9c9bc46db131199737b
Robert Nemeth <nemethrobert1911@gmail.com>
2018-10-11 14:37:06 +0200

    mod: Fund call and receive

--- vendor/nato/milmedfi/CoeMedFiSponsoringNationDAO.php
+++ vendor/nato/milmedfi/CoeMedFiSponsoringNationDAO.php
@@ -6,7 +6,7 @@
  *
  * @author Robert
  * @start   2018-07-08
- * @lastmod 2018-07-11 12:01
+ * @lastmod 2018-10-11 14:41
  *
  */
 
@@ -23,16 +23,29 @@ class CoeMedFiSponsoringNationDAO extends \Jazz\Core\JazzCoreDAO{
 	 //
 	 $m = array('#__natomm_sn'); // map|table
 	 //
-	 $m[] = array( 'id', 'ID' ); // INT(11) ISO 3166 ID#
+	 $m[] = array( 'id', 'ID' ); // INT(11)
 	 
-	 $m[] = array( 'a', 'Active' ); 		// TINYINT(1)
+	 $m[] = array( 'yr', 'Yr' ); // TINYINT Yr e.g. '18
+	 $m[] = array( 'cc', 'CC' ); // INT(11) ISO 3166 ID#
+	 
+	 $m[] = array( 'a', 'Active' ); 		// TINYINT(1) | Active in 'this' year?
+	 
 	 $m[] = array( 'ul', 'UseLeftover' );	// TINYINT(1) Use:1 Retain:0
 	 
 	 $m[] = array( 'sp3', 'SP3' ); 			// CHAR(4) NATO STANAG 1059 Country Code
 	 $m[] = array( 'n', 'CountryName' ); 	// CHAR(24) Plain text
 	 
 	 $m[] = array( 'curr', 'Currency' ); 	// INT | ISO 4217
-	 $m[] = array( 'amtnow', 'AmountNow' );	// DECIMAL(10,2)
+	 
+	 $m[] = array( 'cf1a', 'Call1Amount' );	// DECIMAL(10,2) Call for Fund part 1
+	 $m[] = array( 'rf1a', 'Received1Amount' );	// DECIMAL(10,2) Received Fund part 1
+	 $m[] = array( 'cf2a', 'Call2Amount' );	// DECIMAL(10,2) 
+	 $m[] = array( 'rf2a', 'Received2Amount' );	// DECIMAL(10,2)
+	 
+	 $m[] = array( 'cf1d', 'Call1Date' );	// DATE
+	 $m[] = array( 'rf1d', 'Received1Date' ); 
+	 $m[] = array( 'cf2d', 'Call2Date' ); 
+	 $m[] = array( 'rf2d', 'Received2Date' ); 
 	 //
 	 return $m;
 	 //
